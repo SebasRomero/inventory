3b66d2052316d5bbdfcd9530d93ca71f428d73cb
sebasromero <sebastian_eromero@hotmail.com>
2023-09-10 16:40:37 -0400

    feat:: add entity and models

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/sebasromero/go_inventory/internal/entity"
+)
+
+type Repository interface {
+	SaveUser(ctx context.Context, email, name, password string) error
+	GetUserByEmail(ctx context.Context, email string) entity.User
+}
