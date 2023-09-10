3e6c601f9d95ee6d325195d5c3743cd8256dc453
sebasromero <sebastian_eromero@hotmail.com>
2023-09-09 23:09:24 -0400

    feat:: connection with mariadb succesfull

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/sebasromero/go_inventory/database"
 	"github.com/sebasromero/go_inventory/settings"
 	"go.uber.org/fx"
 )
@@ -8,9 +12,18 @@ import (
 func main() {
 	app := fx.New(
 		fx.Provide(
+			context.Background,
 			settings.New,
+			database.New,
 		),
-		fx.Invoke(),
+		fx.Invoke(
+			func(db *sqlx.DB) {
+				_, err := db.Query("SELECT * FROM USERS")
+				if err != nil {
+					panic(err)
+				}
+			},
+		),
 	)
 
 	app.Run()
